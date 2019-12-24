ca2aab1af3f2351afc23732bbad885951aabe95c
malikfade <57961706+malikfade@users.noreply.github.com>
2019-12-24 16:42:49 +0200

    Add files via upload

---
+++ Testing/edgeData_Testing.java
@@ -0,0 +1,14 @@
+package Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class edgeData_Testing {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
